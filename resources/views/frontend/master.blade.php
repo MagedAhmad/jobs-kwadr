87b74d4d8ba3feeb8fce4ec5eef9ac08e6fefe6d
Maged Raslan <maged.ahmedr@gmail.com>
2021-12-04 16:18:33 +0200

    Remove nav items

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -47,18 +47,7 @@
                     </div>
                 </div>
                 
-                <div class="block sm:hidden bg-white border-t-2 py-2">
-                    <div class="flex flex-col">
-                    <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Products</a>
-                    <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Marketplace</a>
-                    <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Partners</a>
-                    <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Pricing</a>
-                    <div class="flex justify-between items-center border-t-2 pt-2">
-                        <a href="#" class="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Sign in</a>
-                        <a href="#" class="text-gray-800 text-sm font-semibold border px-4 py-1 rounded-lg hover:text-purple-600 hover:border-purple-600">Sign up</a>
-                    </div>
-                    </div>
-                </div>
+                
                 </div>
             </div>
         </div>
